ee836b62ab9a7543396f04e3829f3ba89ee4cbf3
Narayan <narayanbuilds@gmail.com>
2026-09-11 15:01:00 +0530

    update kt field of metal purity

--- src/backend/InvenTree/master/models.py
+++ src/backend/InvenTree/master/models.py
@@ -46,7 +46,7 @@ class MetalPurity(MasterFieldsMixin):
 
     name = models.CharField(max_length=50, verbose_name=_('Name'), help_text=_('Name of the metal purity grade.'))
     metal_type = models.ForeignKey(MetalType, on_delete=models.CASCADE, related_name='purities', verbose_name=_('Metal Type'), help_text=_('Metal type associated with this purity grade.'))
-    purity = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0), MaxValueValidator(100)], verbose_name=_('Purity (%)'), help_text=_('Purity percentage of the metal, from 0 to 100.'))
+    purity = models.PositiveIntegerField(validators=[MinValueValidator(1)], verbose_name=_('Karat'), help_text=_('Karat value of the metal, such as 14, 18, 22.'))
 
     class Meta:
         verbose_name = _('Metal Purity')
